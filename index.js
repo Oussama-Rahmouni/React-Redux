eefefc4b399e4ca862b8c58138079316ca912969
Oussama-Rahmouni <oussama.rahmouni.manager@gmail.com>
2023-12-14 16:12:41 +0100

    reducer done

--- index.js
+++ index.js
@@ -7,3 +7,23 @@ return {
     quantity: 1 
 }
 }
+
+const initialState = {
+
+    numOfCackes : 10,
+    another : 0
+}
+//(previousState, action ) => newState
+const reducer =  (state = initialState, action) =>{
+    switch(action.type){
+        case CACKE_ORDERED:
+            return {
+                //usualy we deal with objects with multiple property so in case we separate the object and only 
+                //update the property that we want to update
+                ...state,
+                numOfCackes: state.numOfCackes - 1
+            }
+        default:
+            return state
+    }
+}
